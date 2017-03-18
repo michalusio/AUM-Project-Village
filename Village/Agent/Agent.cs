e256520b58859b9cc2a9efc69a485482b420fe1c
Michał <Micha@DIDI-KOMPUTER>
2017-03-18 16:58:12 +0100

    Adding an agent class (for now only field, constructur, destructor and getters)

---
+++ Village/Agent/Agent.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Village.Agent
+{
+    class Agent
+    {
+        private int age, food;
+        private double strength, durability, speed;
+
+        private readonly Genes.Genome genome;
+
+        public Agent(List<Tuple<Agent>> agents)
+        {
+            
+        }
+
+        ~Agent()
+        {
+
+        }
+
+        public double getStrength() => strength;
+        public double getDurability() => durability;
+        public double getSpeed() => speed;
+    }
+}
