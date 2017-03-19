e9628393636e9b9a6086e3c8ce3b90afed152e8a
Sachio0 <nowi.king@gmail.com>
2017-03-19 19:56:13 +0100

    robienie tablicy planszy oraz pól uprawnych i jałowych

---
+++ Village/Board/Field.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Village.Board
+{
+    public class Field
+    {
+        private bool _cultivation; // czy uprawa jest możliwa
+        public int eat; //eat = 0 brak jedzenia
+        public bool isAgent;
+        public Field(bool cultivation, int eatingValue)
+        {
+            _cultivation = cultivation;
+            isAgent = false;
+            eat = eatingValue;
+        }
+        public Field(bool cultivation)
+        {
+            _cultivation = cultivation;
+            isAgent = false;
+            eat = 0;
+        }
+        Field()
+        {
+            _cultivation = true;
+            isAgent = false;
+            eat = 0;
+        }
+    }
+}
