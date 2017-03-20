963ee9830563968ab6d02c8c339b8709708ed64c
JohannGambolputty <swannowsky@gmail.com>
2017-03-20 18:01:12 +0100

    Updating Agent.cs
    Removing an unnecessary field with stats (with their accessors), adding coordinates, decrypting C# documentation, creating potential memory leaks.

--- Village/Agent/Agent.cs
+++ Village/Agent/Agent.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -6,18 +6,36 @@ namespace Village.Agent
 {
     class Agent
     {
+        //Coordinates
+        private int [] currentCoordinates = new int[2];
+        private int [] futureCoordinates = new int[2];
+        //Age and Food
         private int age, food;
-        private double strength, durability, speed;
+        //List of genomes
+        private readonly List<Tuple<Genes.Genome, float>> genome;
 
-        private readonly Genes.Genome genome;
-
-        public Agent(List<Tuple<Agent>> agents)
+        public Agent()
+        {
+            Genes.Genome(genome);
+        }
+        
+        //Getters
+        public int getCurrentX => currentCoordinates[0];
+        public int getCurrentY => currentCoordinates[1];
+        public int getAge => age;
+        public int getFood => food;
+        
+        //Setters
+        public void setCoordinates(int X, int Y)
+        {
+            currentCoordinates[0] = X;
+            currentCoordinates[1] = Y;
+        }
+        
+        //Other methods
+        public void doRandomAction()
         {
             
         }
-
-        public double getStrength() => strength;
-        public double getDurability() => durability;
-        public double getSpeed() => speed;
     }
 }
